ab625a564d5abae347a7caa965915407d6b6b26c
Ronald <ronald.bordab@gmail.com>
2021-11-24 22:32:55 -0500

    archivos subidos

---
+++ src/main/java/aplicaciones/spring/Lab15Lab1Application.java
@@ -0,0 +1,13 @@
+package aplicaciones.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab15Lab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab15Lab1Application.class, args);
+	}
+
+}
